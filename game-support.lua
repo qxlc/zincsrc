0435b2ec129ac23e417bf696de44cf7541c47e4c
qxlc <liamq15@outlook.com>
2025-05-26 17:38:44 -0400

    Create game-support.lua

---
+++ game-support.lua
@@ -0,0 +1,93 @@
+return {
+    -- Official & Popular Da Hood Games
+    [2788229376] = {
+        name = "Da Hood",
+        safe = false,
+        hasAntiCheat = true,
+        remotes = {
+            HitPart = "MainEvent",
+            Shoot = "ShootEvent"
+        }
+    },
+
+    [66654135] = {
+        name = "Derhood",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "DerEvent",
+            Shoot = "DerShoot"
+        }
+    },
+
+    [99234567] = {
+        name = "Deehood",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "DeeEvent",
+            Shoot = "DeeShoot"
+        }
+    },
+
+    [88001234] = {
+        name = "Hood Customs",
+        safe = true,
+        hasAntiCheat = true,
+        remotes = {
+            HitPart = "CustomsHit",
+            Shoot = "CustomsShoot"
+        }
+    },
+
+    -- Placeholder Copies
+    [100000001] = {
+        name = "Da Hood Copy 1",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "HitRemote",
+            Shoot = "GunFire"
+        }
+    },
+
+    [100000002] = {
+        name = "Da Hood Copy 2",
+        safe = false,
+        hasAntiCheat = true,
+        remotes = {
+            HitPart = "FireEvent",
+            Shoot = "FireGun"
+        }
+    },
+
+    [100000003] = {
+        name = "Da Hood Copy 3",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "HitPartRemote",
+            Shoot = "RemoteShoot"
+        }
+    },
+
+    [100000004] = {
+        name = "Da Hood Copy 4",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "HitSomething",
+            Shoot = "GunBang"
+        }
+    },
+
+    [100000005] = {
+        name = "Da Hood Copy 5",
+        safe = true,
+        hasAntiCheat = false,
+        remotes = {
+            HitPart = "HPart",
+            Shoot = "Fire"
+        }
+    },
+
